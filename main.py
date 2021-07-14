f22e1f2fddadf5ed870b61aedf7a9acc5a2841ad
Евгений <evgenij@Evgenijs-MacBook-Pro.local>
2021-07-14 19:43:07 +0600

    updated 1st task + add

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("hello i am programmer")
+name = "Yevgeniy"
+print("My name is "+ name)
+a = "103"
+a = int(a)
+b = 301
+not_found = (a + b)
+not_found = str(not_found)
+print(not_found)
+print("not_found " + not_found)
